6962a1efc234ab0e2f81b493473b1567d777b9cd
Kseniya.Edkova <Kseniya.Edkova@tatar.ru>
2022-03-15 22:03:21 +0300

    add demo

--- TestForm.py
+++ TestForm.py
@@ -1,4 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QListWidgetItem
+import sqlite3
+from PyQt5.QtWidgets import QMainWindow
+import webbrowser
+
+from task5 import VectorSearch
 from test_form import Ui_MainWindow
 import string
 import nltk
@@ -14,53 +18,53 @@ class TestFormWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.loadUi()
+        self.con = sqlite3.connect("grouping_db.sqlite")
         self.btn_search.clicked.connect(self.search)
 
     # тут надо описать логику поиска
     def search(self):
-        searching = self.search_text.text()
+        values = VectorSearch(self.search_text.text()).res
+        links = self.get_sites(values)
+        self.result_tet.addItems(links)
+        self.result_tet.itemClicked.connect(self.selectionChanged)
 
-        dict = list(self.get_sites(self.get_binary_search(searching)))
-        i = 0
-        for el in dict:
-            self.list_vidg.insertItem(i, el)
-            i+=1
-
-        # Имя элемента совпадает с objectName в QTDesigner
+    def selectionChanged(self, item):
+        print("Вы кликнули: {}".format(item.text()))
+        webbrowser.open(str(item.text()))
 
     def loadUi(self):
         # uic.loadUi('ui/test_form.ui', self)  # Загружаем дизайн
         self.setupUi(self)
 
-    def preprocess(self, line):
-        result = []
-        line = line.lower()
-        line = line.replace('»', ' ')
-        for t in line:
-            if t in string.punctuation:
-                line = line.replace(t, ' ')
-        line = [t for t in line.split() if t not in stopwords.words('russian')]
-        temp = []
-        for word in line:
-            temp.append(word)
-        result += [temp]
-        dataset = []
-        for item in result:
-            dataset += item
-        r = re.compile("[а-яА-Я]+")
-        dataset = [w for w in filter(r.match, dataset)]
-        return dataset
+    # def preprocess(self, line):
+    #     result = []
+    #     line = line.lower()
+    #     line = line.replace('»', ' ')
+    #     for t in line:
+    #         if t in string.punctuation:
+    #             line = line.replace(t, ' ')
+    #     line = [t for t in line.split() if t not in stopwords.words('russian')]
+    #     temp = []
+    #     for word in line:
+    #         temp.append(word)
+    #     result += [temp]
+    #     dataset = []
+    #     for item in result:
+    #         dataset += item
+    #     r = re.compile("[а-яА-Я]+")
+    #     dataset = [w for w in filter(r.match, dataset)]
+    #     return dataset
 
-    def get_words_indexes(self):
-        with open('inverted_indexes.txt', encoding="utf-8") as file:
-            name = [row.strip() for row in file]
-
-        count_words = {}
-        for elem in name:
-            elem = elem.replace('\'', '\"')
-            dictData = json.loads(elem)
-            count_words[dictData["word"]] = dictData["inverted_array"]
-        return count_words
+    # def get_words_indexes(self):
+    #     with open('inverted_indexes.txt', encoding="utf-8") as file:
+    #         name = [row.strip() for row in file]
+    #
+    #     count_words = {}
+    #     for elem in name:
+    #         elem = elem.replace('\'', '\"')
+    #         dictData = json.loads(elem)
+    #         count_words[dictData["word"]] = dictData["inverted_array"]
+    #     return count_words
 
     def get_sites(self, site_ids):
         with open('index.txt', encoding="utf-8") as file:
@@ -74,23 +78,25 @@ class TestFormWidget(QMainWindow, Ui_MainWindow):
             res[id] = sites[id]
         return res.values()
 
-    def get_binary_search(self, line):
-        dataset = self.preprocess(line)
-        count_words = self.get_words_indexes()
+    # def get_binary_search(self, line):
+    #     dataset = self.preprocess(line)
+    #     count_words = self.get_words_indexes()
+    #
+    #     mas_res = {}
+    #     for word in dataset:
+    #         if word in count_words.keys():
+    #             mas_res[word] = count_words[word]
+    #         else:
+    #             mas_res[word] = []
+    #
+    #     i = 0
+    #     res = []
+    #     for elem in mas_res:
+    #         i += 1
+    #         if i == 1:
+    #             res = mas_res[elem]
+    #         else:
+    #             res = list(set(res) & set(mas_res[elem]))
+    #     return res
 
-        mas_res = {}
-        for word in dataset:
-            if word in count_words.keys():
-                mas_res[word] = count_words[word]
-            else:
-                mas_res[word] = []
 
-        i = 0
-        res = []
-        for elem in mas_res:
-            i += 1
-            if i == 1:
-                res = mas_res[elem]
-            else:
-                res = list(set(res) & set(mas_res[elem]))
-        return res
--- task5.py
+++ task5.py
@@ -1,7 +1,4 @@
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import nltk
-from pymorphy2 import MorphAnalyzer
+
 import numpy as np
 import re
 from string import punctuation
@@ -13,64 +10,68 @@ from nltk.corpus import stopwords
 import pymorphy2
 
 nltk.download('stopwords')
+class VectorSearch():
+    def __init__(self, value):
+        self.search_string = value
+        self.lemms, self.matrix = self.load_index()
+        self.res = self.search(self.search_string, self.lemms, self.matrix)
 
-def preprocess(line):
-    line = line.lower()
-    line = line.replace('»', ' ')
-    for t in line:
-        if t in string.punctuation:
-            line = line.replace(t, ' ')
-    line = [t for t in line.split() if t not in stopwords.words('russian')]
-    temp = []
-    for word in line:
-        temp.append(word)
-    return temp
+    def preprocess(self, line):
+        line = line.lower()
+        line = line.replace('»', ' ')
+        for t in line:
+            if t in string.punctuation:
+                line = line.replace(t, ' ')
+        line = [t for t in line.split() if t not in stopwords.words('russian')]
+        temp = []
+        for word in line:
+            temp.append(word)
+        return temp
 
-def load_index():
-    with open('test_lemms_stat.txt', 'r', encoding='utf-8') as file:
-        lines = [row.strip() for row in file]
+    def load_index(self):
+        with open('test_lemms_stat.txt', 'r', encoding='utf-8') as file:
+            lines = [row.strip() for row in file]
 
-    lemmas_list = list()
-    matrix = [[0] * 100 for _ in range(len(lines))]
-    idx = 0
-    for line in lines:
-        line = line.replace('\'', '\"')
-        dictData = json.loads(line)
-        lemma = dictData["word"]
+        lemmas_list = list()
+        matrix = [[0] * 100 for _ in range(len(lines))]
+        idx = 0
+        for line in lines:
+            line = line.replace('\'', '\"')
+            dictData = json.loads(line)
+            lemma = dictData["word"]
 
-        lemmas_list.append(lemma)
-        pages = dictData["inverted_array"]
-        for page in pages:
-            matrix[idx][int(page) - 1] = 1
-        idx+=1
-    return lemmas_list, np.array(matrix).transpose()
+            lemmas_list.append(lemma)
+            pages = dictData["inverted_array"]
+            for page in pages:
+                matrix[idx][int(page) - 1] = 1
+            idx+=1
+        return lemmas_list, np.array(matrix).transpose()
 
-def get_vector(search_string, lemmas):
-    morph = pymorphy2.MorphAnalyzer(lang='ru')
-    search_tokens = preprocess(search_string)
-    tokens_normal_form = [morph.parse(token)[0].normal_form for token in search_tokens]
-    vector = [0] * len(lemmas)
-    for token in tokens_normal_form:
-        if token in lemmas:
-            vector[lemmas.index(token)] = 1
-    return vector
+    def get_vector(self, search_string, lemmas):
+        morph = pymorphy2.MorphAnalyzer(lang='ru')
+        search_tokens = self.preprocess(search_string)
+        tokens_normal_form = [morph.parse(token)[0].normal_form for token in search_tokens]
+        vector = [0] * len(lemmas)
+        for token in tokens_normal_form:
+            if token in lemmas:
+                vector[lemmas.index(token)] = 1
+        return vector
 
-def search(search_string, lemmas, matrix):
-    vector = get_vector(search_string, lemmas)
+    def search(self, search_string, lemmas, matrix):
+        vector = self.get_vector(search_string, lemmas)
 
-    docs = dict()
-    for idx, doc in enumerate(matrix):
-        if max(doc) == 1:
-            docs[idx + 1] = 1 - spatial.distance.cosine(vector, doc)
-        else:
-            docs[idx + 1] = 0.0
+        docs = dict()
+        for idx, doc in enumerate(matrix):
+            if max(doc) == 1:
+                docs[idx + 1] = 1 - spatial.distance.cosine(vector, doc)
+            else:
+                docs[idx + 1] = 0.0
 
-    sorted_docs = sorted(docs.items(), key=lambda x: x[1], reverse=True)
-
-    return sorted_docs
-
-lemmas, matrix = load_index()
-print(search('Звездные войны', lemmas, matrix))
+        sorted_docs = sorted(docs.items(), key=lambda x: x[1], reverse=True)
+        res = []
+        for elem in sorted_docs:
+            res.append(elem[0])
+        return res
 
 
 
